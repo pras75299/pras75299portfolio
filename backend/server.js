d9ad693c58395b38af3b3958cee6c3c8ad971e8f
pras75299 <singh.prashantking@gmail.com>
2025-01-15 19:38:37 +0530

    update the cors

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,8 @@ connectDB();
 app.use(express.json());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin: "https://codexprashantsingh.netlify.app/",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
   })
 );
